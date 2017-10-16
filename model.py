84b6b77c7707449d712aa9d170058ff104542f11
Liang Zhang (CC/ENA) <liang.zhang8@cn.bosch.com>
2017-10-16 11:13:54 +0800

    add the first succesful version

--- model.py
+++ model.py
@@ -37,16 +37,26 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Activation, Lambda, Cropping2D, Dropout
+from keras.layers.convolutional import Convolution2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5 , input_shape = (160, 320, 3) ))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))	
+model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Convolution2D(64,3,3, activation="relu"))	  
 model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dropout(0.5))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split =0.2, shuffle = True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split =0.2, shuffle = True, nb_epoch=5)
 
 model.save('model.h5')
   
